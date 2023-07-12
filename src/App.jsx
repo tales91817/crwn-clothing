1554fe323abfa40f7bced191022d0bf383f7f136
tales91817 <tales91817@hotmail.com>
2023-07-12 19:40:36 +0800

    1. add product card 2. add products context 3. add card icon

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
-
-const Shop = () => {
-  return (
-    <div>
-      <h1>I am the Shop page</h1>
-    </div>
-  );
-};
+import Shop from "./components/shop/shop.component";
 
 const App = () => {
   return (
